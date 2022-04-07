3200b38222eee31ce3eb1334b08f0db13dae22d4
AngelNBazan <angelbazan150@gmail.com>
2022-04-07 12:18:00 -0700

    Basic Socket

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var routes = require("./routes");
+let http = require('http');
+
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/js', express.static('./public/js'));
-app.use(express.static('./public/images'));
+app.use('/',express.static('./public'));
 app.use(routes);
+
 app.use('/favicon.ico', express.static('public/test.gif'));
 
+////////Socekt Code//////////
+
+let server = http.createServer(app);
+let io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('update', function (data) {
+        io.emit('update', data);
+    });
+
+});
+//////////////////////////////
+
+
+
 var port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port);
