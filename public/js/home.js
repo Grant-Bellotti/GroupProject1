c239b32e3e4e00a77a0f9fe7ede81bae96edf247
TankerEEE <55513078+TankerEEE@users.noreply.github.com>
2022-04-08 11:39:50 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -3,18 +3,22 @@ let ident;
 let socket = io();
 //Get message from server.
 socket.on('welcome', function(data) {
-      ident = data.id;
+      ident = data.message;
       console.log(ident);
-      $("#messages").append('<li>' + data.message + " " + data.id + '</li>');
+      //$("#messages").append('<li>' + data.message + " " + data.id + '</li>');
 });
 
 //Get message from server.
 socket.on('update', (data) => {
-      $("#messages").append('<li>' + data.ident + '</li>');
+    console.log(data.ident);
 });
 
 function doit() {
 //Send message to server.
+    ident = $("#chat").val();
+    const p = document.createElement("li");
+      p.append(ident);
+      $("#messages").append(p);
       socket.emit('update', {'ident': ident});
       return false;
 }
