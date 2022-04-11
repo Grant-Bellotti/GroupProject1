5d800326abb8ab4051fa6a791882676fd8c4fb8e
TankerEEE <55513078+TankerEEE@users.noreply.github.com>
2022-04-11 13:08:09 -0700

    Made changes to html css and to home.js

--- public/js/home.js
+++ public/js/home.js
@@ -9,7 +9,23 @@ socket.on('welcome', function(data) {
 
 //Get message from server.
 socket.on('update', (data) => {
-      $("#messages").append('<li>' + data.user +": "+data.msg + '</li>');
+ let para = document.createElement("div");
+if(data.msg == "")
+ return;
+
+$("#messages").append(
+'<li>' + data.msg + " " + data.user + '</li>'+
+"<button type="+ "button"+
+" class="+ "collapsible"+
+//"onclick=" +"collapseIt()"+
+">Comments</button>"+
+
+"<div class="+ "content"+"> " +"<hr>"
+  +"<p>Lorem ipsum...</p>" +"<hr>"
++"</div>"
+);
+collapseIt();
+
 });
 
 function doit() {
@@ -18,7 +34,22 @@ function doit() {
       socket.emit('update', {'msg': msg,'user':ident});
       return false;
 }
+function collapseIt(){
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+}
 $(document).ready(function(){
 
 });
