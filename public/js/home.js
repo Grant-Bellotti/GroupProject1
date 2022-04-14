c7ab3c77b04a9aa9d2106f560f144ea0458a586b
AngelNBazan <angelbazan150@gmail.com>
2022-04-14 12:57:31 -0700

    Updated HTML
    Updated how html looks

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,10 @@
 let ident;
-let messageid = 1;
+
 let socket = io();
 //Get message from server.
 socket.on('welcome', function(data) {
       ident = data.id;
-      //$("#messages").append('<p>' + data.message + " " + data.id + '</p>');
+      $("#messages").append('<li>' + data.message + " " + data.id + '</li>');
 });
 
 //Get message from server.
@@ -12,39 +12,20 @@ socket.on('update', (data) => {
  let para = document.createElement("div");
 if(data.msg == "")
  return;
-let message =  messageid.toString();
-let user = data.user.toString();
-let abc = " ";
+
 $("#messages").append(
-
-"<fieldset>"+
-'<p>'+ " Username: " + user +" "+  data.msg + '</p>'+
-
+'<p class="postli">' + data.msg + " " + data.user + '</p>'+
 "<button type="+ "button"+
 " class="+ "collapsible"+
-" id =" + messageid +
 //"onclick=" +"collapseIt()"+
 ">Comments</button>"+
 
-"<div id =" + "d"+ messageid + " class="+ "content"+"> " +"<hr>"
-  +"<ul id=" + "p"+ messageid + "></ul>" + "<br>"
-  + "<input id =" + "t" + messageid + " type="+ "text"+">"
-  +"<input id =" + "c"+ messageid + " type=button name=commentb value=PostComment onclick= " + "commentit("+  messageid + ")>" +"<br>"
+"<div class="+ "content"+"> " +"<hr>"
+  +"<p>Lorem ipsum...</p>" +"<hr>"
 +"</div>"
-+"</fieldset>"
 );
+collapseIt();
 
-//document.getElementById("c"+message).addEventListener("click",commentit(messageid));
-collapseIt(message);
-messageid++;
-
-//collapseIt(messageid);
-
-});
-
-socket.on('updateComments',(data) =>
-{
-$("#"+"p"+data.messageID).append("<p> "+ data.user + " " + data.text +  " <p>");
 });
 
 function doit() {
@@ -69,35 +50,12 @@ function doit() {
 
       return false;
 }
-function commentit(id){
-let text = $("#"+"t"+id).val();
-let user =  $('#tempUser').val();
-let pw = $('#password').val();
-if(text != ""){
-  $.ajax({
-            url: "/check",
-            type: "GET",
-            data: {username:user,password:pw},
-            success: function(data){
-                if (data.error){
-                  alert(data.message);
-                }
-                else {
-                  socket.emit('updateComments', {'text': text,'messageID':id,'user': user});
-                }
-              } ,
-            dataType: "json"
-          });
-  }
-  else
-    alert("you need a message");
-}
 
-
-function collapseIt(data){
-//let message =  data.toString();
-
-  document.getElementById(data).addEventListener("click", function() {
+function collapseIt(){
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -106,8 +64,9 @@ function collapseIt(data){
       content.style.display = "block";
     }
   });
-
 }
+}
+
 function showPassword() {
   var input = document.getElementById("password");
   if (input.type === "password") {
